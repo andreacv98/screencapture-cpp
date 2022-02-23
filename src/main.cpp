da61795343689c7bdd49ec032aa2b2152b06686c
teodoro98 <teodosiobla@gmail.com>
2022-02-23 10:16:00 +0100

    added menu for select area

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,10 @@ int main() {
     int audio_number;
     char output_name[100];
     int resolution_out;
+    int x_start;
+    int x_toadd;
+    int y_start;
+    int y_toadd;
 
 
     std::cout << "\n\nStarted!\n\n" << std::endl;
@@ -34,9 +38,41 @@ int main() {
     sc.settings._recvideo = true;
 
 
+#ifdef __unix__
     std::cout << "---Select screen # to record---" << std::endl;
-    //sc.infoDisplays();
+    sc.infoDisplays();
     std::cin >>screen_number;
+#endif
+
+
+    std::cout << "---Enter n pixel to start area capture x (x offset) ---" << std::endl;
+    std::cin >>x_start;
+    std::cout << "---Enter n pixel to start area capture y (y offset) ---" << std::endl;
+    std::cin >>y_start;
+    std::cout << "---Enter n pixel to add to start area capture x (x added) ---" << std::endl;
+    std::cin >>x_toadd;
+    std::cout << "---Enter n pixel to add to start area capture y (y added) ---" << std::endl;
+    std::cin >>y_toadd;
+
+
+#ifdef _WIN32
+    sc.settings._screenoffset = {x_start, y_start};
+    sc.settings._inscreenres= {x_toadd, y_toadd};
+#endif
+
+#ifdef __unix__
+    sc.settings._screenoffset = {x_start, y_start};
+    sc.settings._inscreenres= {1280, 720};
+#endif
+
+/*
+#ifdef __unix__
+    sc.settings._inscreenres = {XDisplayWidth(sc.dpy,screen_number),  XDisplayHeight(sc.dpy,screen_number)};
+#endif
+#ifdef _WIN32
+    sc.settings._inscreenres = {2560,1440};
+#endif
+    */
 
     std::cout << "---Do you want audio record? 1 for yes, 0 for not ---" << std::endl;
     std::cin >>audio;
@@ -55,15 +91,10 @@ int main() {
 
     sc.settings.filename = output_name;
 
-#ifdef __unix__
-    sc.settings._inscreenres = {XDisplayWidth(sc.dpy,screen_number),  XDisplayHeight(sc.dpy,screen_number)};
-#endif
-
-    //sc.settings._inscreenres = {XDisplayWidth(sc.dpy,screen_number),  XDisplayHeight(sc.dpy,screen_number)};
-
     std::cout << "---Choose output resolution #" << std::endl;
     std::cout << "resolution #0: \t1280x720 pixels"
-                 "\nresolution #1: \t1920x1080 pixels"<< std::endl;
+                 "\nresolution #1: \t1920x1080 pixels"<<
+                 "\nresolution #2: \t2560x1440 pixels"<< std::endl;
     std::cin >> resolution_out;
 
     switch (resolution_out) {
@@ -73,13 +104,14 @@ int main() {
         case 1:
             sc.settings._outscreenres = {1920,1080};
             break;
+        case 2:
+            sc.settings._outscreenres = {2560,1440};
+            break;
         default:
             break;
     }
 
-    sc.settings._fps = 14;
-
-    sc.settings._screenoffset = {0,0};
+    sc.settings._fps = 15;
 
     /*open input devices*/
     sc.openVideoSource();
