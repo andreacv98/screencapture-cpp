fa1292cb1571e5f38b1942dd36d226c8cdc261a5
teodoro98 <teodosiobla@gmail.com>
2022-02-23 10:16:34 +0100

    modification of capture audio device

--- src/ScreenRecorder.h
+++ src/ScreenRecorder.h
@@ -15,6 +15,7 @@
 //#include <semaphore.h>
 
 #ifdef WIN32
+#include <windows.h>
 #endif
 
 #ifdef __unix__
@@ -67,7 +68,7 @@ extern "C"
 #define VIDEO_SOURCE ("gdigrab")
 #define VIDEO_URL ("desktop")
 #define AUDIO_SOURCE ("dshow")
-#define AUDIO_URL ("audio=Microfono (USB Microphone)")
+#define AUDIO_URL ("audio=Microfono (Logitech G533 Gaming Headset)")
 #endif
 
 #define CAPTURE_BUFFER 10
